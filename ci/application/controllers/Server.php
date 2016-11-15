154605f4a6e584da70bb44db3661905ad61250bb
LarryShi <szq1991@gmail.com>
2016-11-15 23:46:20 +0800

    More Detail.

--- ci/application/controllers/Server.php
+++ ci/application/controllers/Server.php
@@ -47,7 +47,7 @@ class Server extends CI_Controller {
 		//传过来的是JSON String，用下面这句
     	//$json = json_decode($json_string, true);
     	//传过来的是JSON Object，用下面这句
-    	
+    
 		$json = $json_string;
 		$paramemter['UserName'] = $json['UserName'];
         $paramemter['Password'] = md5($json['Password']);
@@ -59,20 +59,23 @@ class Server extends CI_Controller {
         $paramemter['WalkDistance']='0';
         $paramemter['GpsLat']='0';
         $paramemter['GpsLon']='0';
-		$result = $this->user_model->register($paramemter);
-		if($result['ret'] === 200)
-		{
-			$data['code'] = 0;
-			$data['message'] = "Successful";	
-		}
-		else if($result['ret'] === 401)
-		{
-			$data['code'] = 2;
-			$data['message'] = "Used UserName";
-		}
-		else{
-			$data['code'] = 3;
-			$data['message'] = "Fail";		
+        $data['code']="1";
+        if($paramemter['UserName']!=""&&$paramemter['Password']!=""){
+			$result = $this->user_model->register($paramemter);
+			if($result['ret'] === 200)
+			{
+				$data['code'] = 0;
+				$data['message'] = "Successful";	
+			}
+			else if($result['ret'] === 401)
+			{
+				$data['code'] = 2;
+				$data['message'] = "Used UserName";
+			}
+			else{
+				$data['code'] = 3;
+				$data['message'] = "Fail";		
+			}
 		}
 		$content_data['display_value']['Link']="http://i.cs.hku.hk/~zqshi/ci/index.php/Server/registerM";
 		
@@ -103,31 +106,34 @@ class Server extends CI_Controller {
         $paramemter['WalkDistance']='0';
         $paramemter['GpsLat']='0';
         $paramemter['GpsLon']='0';
-		$result = $this->user_model->register($paramemter);
-		if($result['ret'] === 200)
-		{
-			$data['code'] = 0;
-			$data['message'] = "Successful";
-			$this->output
-        			->set_content_type('application/json')
-        			->set_output(json_encode($data));
-			
-		}
-		else if($result['ret'] === 401)
-		{
-			$data['code'] = 2;
-			$data['message'] = "Used UserName";
-			$this->output
-        			->set_content_type('application/json')
-        			->set_output(json_encode($data));
-		}
-		else{
-			$data['code'] = 3;
-			$data['message'] = "Fail";
-			$this->output
-        			->set_content_type('application/json')
-        			->set_output(json_encode($data));
-			
+        $data['code']="1";
+        if($paramemter['UserName']!=""&&$paramemter['Password']!=""){
+			$result = $this->user_model->register($paramemter);
+			if($result['ret'] === 200)
+			{
+				$data['code'] = 0;
+				$data['message'] = "Successful";
+				$this->output
+	        			->set_content_type('application/json')
+	        			->set_output(json_encode($data));
+				
+			}
+			else if($result['ret'] === 401)
+			{
+				$data['code'] = 2;
+				$data['message'] = "Used UserName";
+				$this->output
+	        			->set_content_type('application/json')
+	        			->set_output(json_encode($data));
+			}
+			else{
+				$data['code'] = 3;
+				$data['message'] = "Fail";
+				$this->output
+	        			->set_content_type('application/json')
+	        			->set_output(json_encode($data));
+				
+			}
 		}
 	}
 
@@ -139,29 +145,33 @@ class Server extends CI_Controller {
 		//传过来的是JSON String，用下面这句
     	//$json = json_decode($json_string, true);
     	//传过来的是JSON Object，用下面这句
-    	$json2 = json_encode($json_string, true);
 		$json = $json_string;
+
 		$paramemter['UserName'] = $json['UserName'];
         $paramemter['Password'] = md5($json['Password']);
-		$result = $this->user_model->login($paramemter);
-		if($result['ret']==200)
-		{
-		
-			$data['code'] = 0;
-			$data['UserInfo']=$result['UserInfo'];
+		$data['code']="1";
+		if($paramemter['UserName']!=""&&$paramemter['Password']!=""){
+			$result = $this->user_model->login($paramemter);
+			if($result['ret']==200)
+			{
+			
+				$data['code'] = 0;
+				$data['UserInfo']=$result['UserInfo'];
 
-		}else{
-			$data['code']=2;
-			$data['message'] = "UserName or Password not Match";
+			}else{
+				$data['code']=2;
+				$data['message'] = "UserName or Password not Match";
 
+			}
 		}
 		$content_data['display_value']['Link']="http://i.cs.hku.hk/~zqshi/ci/index.php/Server/loginM";
 		
 		$content_data['display_value']['Input']=json_encode($json_string, true);
 
-		$content_data['display_value']['Return']=json_encode($data);
+		$content_data['display_value']['Return']=json_encode($data, true);
 
 		$this->load->view('result',$content_data);
+
 	}
 
 	public function loginM()
@@ -175,22 +185,25 @@ class Server extends CI_Controller {
 		//$json = $json_string;
 		$paramemter['UserName'] = $json['UserName'];
         $paramemter['Password'] = md5($json['Password']);
-     
-		$result = $this->user_model->login($paramemter);
-		if($result['ret']==200)
-		{
-		
-			$data['code'] = 0;
-			$data['UserInfo']=$result['UserInfo'];
-			$this->output
-        			->set_content_type('application/json')
-        			->set_output(json_encode($data));
-		}else{
-			$data['code']=2;
-			$data['message'] = "UserName or Password not Match";
-			$this->output
-        			->set_content_type('application/json')
-        			->set_output(json_encode($data));
+        $data['code']="1";
+        if($paramemter['UserName']!=""&&$paramemter['Password']!=""){
+
+			$result = $this->user_model->login($paramemter);
+			if($result['ret']==200)
+			{
+			
+				$data['code'] = 0;
+				$data['UserInfo']=$result['UserInfo'];
+				$this->output
+	        			->set_content_type('application/json')
+	        			->set_output(json_encode($data));
+			}else{
+				$data['code']=2;
+				$data['message'] = "UserName or Password not Match";
+				$this->output
+	        			->set_content_type('application/json')
+	        			->set_output(json_encode($data));
+			}
 		}
 	}
 }
