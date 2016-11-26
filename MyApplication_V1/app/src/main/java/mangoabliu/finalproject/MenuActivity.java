fd24b30ea376bfa45e29b8d229bbf68c3bd3d0cb
LyrisXu <lyrisxu@gmail.com>
2016-11-26 21:22:33 +0800

    welcome_flash
    welcome_flash

--- MyApplication_V1/app/src/main/java/mangoabliu/finalproject/MenuActivity.java
+++ MyApplication_V1/app/src/main/java/mangoabliu/finalproject/MenuActivity.java
@@ -6,22 +6,83 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.Animation.AnimationListener;
 import android.widget.Button;
+import android.widget.TextView;
 
-public class MenuActivity extends AppCompatActivity {
+public class MenuActivity extends AppCompatActivity implements AnimationListener {
 
     Button btn_Switch;
+
+    TextView tv_Menu;
+    //    Point size;
+//    @TargetApi(16)
+//    Animation animationFadeIn, animationFadeOut;
+    Animation anim = new AlphaAnimation(0.0f, 1.0f);
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         // FULLSCREEN  /LYRIS 11.26
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         setContentView(R.layout.activity_menu);
         btn_Switch = (Button) findViewById(R.id.btn_SwitchLogin);
         btn_Switch.setOnClickListener(new bt_SwitchListener());
+        tv_Menu = (TextView) findViewById(R.id.tv_menu);
+
+        // translation animate -deleted
+////        tv_Menu.setOnClickListener(new tv_MenuListener());
+//        //get screen size
+//        size=new Point();
+//        Display display=getWindowManager().getDefaultDisplay();
+//        display.getSize(size);
+//        tv_Menu.animate().translationX(size.y/2).withStartAction(new Runnable(){
+//            @Override
+//            public void run() {
+//                tv_Menu.animate().translationY(size.y/4).alpha(0.5f);
+//            }
+//        });
+
+        //slide animate -deleted
+//        tv_Menu.startAnimation(AnimationUtils.loadAnimation(MenuActivity.this,android.R.anim.slide_in_left));
+
+        //fade_In
+//        animationFadeIn= AnimationUtils.loadAnimation(MenuActivity.this,R.anim.fade_in);
+//        animationFadeIn.setRepeatCount(10);
+//        animationFadeOut=AnimationUtils.loadAnimation(MenuActivity.this,R.anim.fade_out);
+//        tv_Menu.setVisibility(View.VISIBLE);
+//        tv_Menu.setAnimation(animationFadeIn);
+
+        //blink
+        anim.setDuration(1500); //You can manage the blinking time with this parameter
+        anim.setStartOffset(20);
+        anim.setRepeatMode(Animation.REVERSE);
+        anim.setRepeatCount(Animation.INFINITE);
+        tv_Menu.startAnimation(anim);
+
+
+
+    }
+
+    @Override
+    public void onAnimationStart(Animation animation) {
+
+    }
+
+    @Override
+    public void onAnimationEnd(Animation animation) {
+
+    }
+
+    @Override
+    public void onAnimationRepeat(Animation animation) {
+
     }
 
 
@@ -32,4 +93,5 @@ public class MenuActivity extends AppCompatActivity {
             startActivityForResult(myIntent, 0);
         }
     }
+
 }
