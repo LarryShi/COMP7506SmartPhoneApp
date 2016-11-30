0b55db67731c49b2cc3b62bca1fb196ad05e3b84
LyrisXu <lyrisxu@gmail.com>
2016-11-30 22:19:47 +0800

    简直蠢啊。。。
    简直蠢啊。。。

--- MyApplication_V1/app/src/main/java/mangoabliu/finalproject/SettingDialog.java
+++ MyApplication_V1/app/src/main/java/mangoabliu/finalproject/SettingDialog.java
@@ -2,7 +2,9 @@ package mangoabliu.finalproject;
 
 import android.app.Dialog;
 import android.content.Context;
+import android.media.AudioManager;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ImageButton;
@@ -23,11 +25,13 @@ public class SettingDialog extends Dialog {
     private int maxVolume;
     private int currentVolume;
     private int muteFlag,cv;
-//    public AudioManager audioManager;
+    public AudioManager audioManager;
+    Context con;
 
 
     protected SettingDialog (Context context, int style) {
         super(context,style);
+        con = context;
         gameModel= GameModel.getInstance();
     }
 
@@ -41,44 +45,74 @@ public class SettingDialog extends Dialog {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.dialog_setting);
 
+        SettingInit();
+
     }
 
     public void SettingInit(){
 
         btnMusicCtrl = (ImageButton) findViewById(R.id.btnMute);
         seekbar = (SeekBar) findViewById(R.id.seekBar);
-//        audioManager = (AudioManager)getSystemService(Context.AUDIO_SERVICE);
-//        maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
-//        cv=currentVolume = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
-//        seekbar.setMax(maxVolume);
-//        seekbar.setProgress(currentVolume);
+        audioManager = (AudioManager) con.getSystemService(Context.AUDIO_SERVICE);
+        maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        cv=currentVolume = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+        seekbar.setMax(maxVolume);
+        seekbar.setProgress(currentVolume);
 
-//        //unmute、mute切换
-//        if(cv!=0) {btnMusicCtrl.setBackgroundResource(R.drawable.unmute);}
-//        else {btnMusicCtrl.setBackgroundResource(R.drawable.mute);}
-//
+        //unmute、mute切换
+        if(cv!=0) {btnMusicCtrl.setBackgroundResource(R.drawable.unmute);}
+        else {btnMusicCtrl.setBackgroundResource(R.drawable.mute);}
+
+        btnMusicCtrl.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                isMute();
+            }
+        });
+
+        seekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC,i,0);
+                currentVolume = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+                if(i>0) {
+                    btnMusicCtrl.setBackgroundResource(R.drawable.unmute);
+                    seekbar.setProgress(i);}
+                else  btnMusicCtrl.setBackgroundResource(R.drawable.mute);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
 
     }
 
 
 
 
-//    public void isMute(){
-//        //按静音键,muteFlag=1说明静音状态
-//        if (currentVolume!=0 || muteFlag==0){
-//            muteFlag=1;
-//            audioManager = (AudioManager)getSystemService(Context.AUDIO_SERVICE);
-//            cv = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
-//            audioManager.setStreamVolume(AudioManager.STREAM_MUSIC,AudioManager.ADJUST_MUTE,0);
-//            btnMusicCtrl.setBackground(getResources().getDrawable(R.drawable.mute));
-//            seekbar.setProgress(0);
-//        }else//按开启键
-//        {
-//            muteFlag=0;
-//            audioManager.setStreamVolume(AudioManager.STREAM_MUSIC,AudioManager.ADJUST_UNMUTE,0);
-//            btnMusicCtrl.setBackground(getResources().getDrawable(R.drawable.unmute));
-//            seekbar.setProgress(cv);
-//        }
-//    }
+    public void isMute(){
+        //按静音键,muteFlag=1说明静音状态
+        if (currentVolume!=0 || muteFlag==0){
+            muteFlag=1;
+            audioManager = (AudioManager) con.getSystemService(Context.AUDIO_SERVICE);
+            cv = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+            audioManager.setStreamVolume(AudioManager.STREAM_MUSIC,AudioManager.ADJUST_MUTE,0);
+            btnMusicCtrl.setBackgroundResource(R.drawable.mute);
+            seekbar.setProgress(0);
+        }else//按开启键
+        {
+            muteFlag=0;
+            audioManager.setStreamVolume(AudioManager.STREAM_MUSIC,AudioManager.ADJUST_UNMUTE,0);
+            btnMusicCtrl.setBackgroundResource(R.drawable.unmute);
+            seekbar.setProgress(cv);
+        }
+    }
 
 }
